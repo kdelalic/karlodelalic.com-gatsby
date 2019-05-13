e36a1cf9a59bbedf1414c3ecbefd3b1fee6575de
Karlo Delalic <karlo.delalic1@gmail.com>
2019-05-12 23:37:47 -0400

    Dynamically load menu links

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,57 @@
-import { Link } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
 import React from "react"
 
 const Header = () => (
-  <header>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <ul>
-        <li>
-            <Link
-                to="/"
+    <StaticQuery
+        query={graphql`
+            query PageInfoQuery{
+                allFile(filter: { sourceInstanceName: { eq: "pages" } }) {
+                    edges {
+                        node {
+                            name
+                            id
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <header>
+                <div
                 style={{
-                color: `white`,
-                textDecoration: `none`,
+                    margin: `0 auto`,
+                    maxWidth: 960,
+                    padding: `1.45rem 1.0875rem`,
                 }}
-            >
-            test
-            </Link>
-        </li>
-      </ul>
-      <h1 style={{ margin: 0 }}>
-        
-      </h1>
-    </div>
-  </header>
+                >
+                <ul style={{ display: 'flex', flex: 1, listStyle: 'none' }}>
+                    {data.allFile.edges
+                        .filter(edge => {
+                            return edge.node.name !== "404" 
+                        })
+                        .map(edge => {
+                            let link = "/";
+                            let name = "Home"
+                            if (edge.node.name !== "index") {
+                                link += edge.node.name;
+                                name = edge.node.name.charAt(0).toUpperCase() + edge.node.name.slice(1);
+                            }
+
+                            return (
+                                <li key={edge.node.id}>
+                                    <Link to={link}>{name}</Link>
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+                <h1 style={{ margin: 0 }}>
+                    
+                </h1>
+                </div>
+            </header>
+        )}
+    />
 )
 
 export default Header
