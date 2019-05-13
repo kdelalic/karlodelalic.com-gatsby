1f877746d06349c881e8db7fdec37e724d2a5003
Karlo Delalic <karlo.delalic1@gmail.com>
2019-05-12 22:09:06 -0400

    Add basic layout, plugins

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,8 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+const Header = () => (
+  <header>
     <div
       style={{
         margin: `0 auto`,
@@ -16,27 +10,24 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
+      <ul>
+        <li>
+            <Link
+                to="/"
+                style={{
+                color: `white`,
+                textDecoration: `none`,
+                }}
+            >
+            test
+            </Link>
+        </li>
+      </ul>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+        
       </h1>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
