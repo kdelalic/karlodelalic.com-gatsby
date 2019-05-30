88b68437db4630ff92724b9b834f91c1ca771fd3
Karlo Delalic <karlo.delalic@ibm.com>
2019-05-29 23:00:55 -0400

    Responsive nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, Link, StaticQuery } from "gatsby"
 
 import Logo from "../assets/images/logo.svg";
 
 import "./header.scss"
 
-const Header = () => (
-  <StaticQuery
-    query={graphql`
+const Header = () => {
+
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <StaticQuery
+      query={graphql`
             query PageInfoQuery {
                 allFile(
                   filter: { 
@@ -23,40 +27,46 @@ const Header = () => (
                 }
             }
         `}
-    render={data => (
-      <header>
-        <div className="brand">
-          <Link to={"/"} className="brand__logo">
-            <Logo />
-          </Link>
-          <span className="brand__name">Karlo Delalic</span>
-        </div>
-        <nav className="navbar">
-          <Link to={"/"} className="navbar__item" activeClassName="active">Home</Link>
-          {data.allFile.edges
-            .filter(edge => {
-              return edge.node.name !== "404" && edge.node.name !== "index"
-            })
-            .map(edge => {
-              const link = `/${edge.node.name}`;
-              const name = edge.node.name.charAt(0).toUpperCase() + edge.node.name.slice(1);
-              return (
-                <Link
-                  key={edge.node.id}
-                  to={link}
-                  className="navbar__item"
-                  activeClassName="active"
-                  partiallyActive={true}
-                >
-                  {name}
-                </Link>
-              )
-            })
-          }
-        </nav>
-      </header>
-    )}
-  />
-)
+      render={data => (
+        <header>
+          <div className="brand">
+            <Link to={"/"} className="brand__logo">
+              <Logo />
+            </Link>
+            <span className="brand__name">Karlo Delalic</span>
+          </div>
+          <div onClick={() => setNavbarOpen(!isNavbarOpen)} className={`navbar-burger${isNavbarOpen ? " open" : ""}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav className={`navbar${isNavbarOpen ? " open" : ""}`}>
+            <Link to={"/"} className="navbar__item" activeClassName="active">Home</Link>
+            {data.allFile.edges
+              .filter(edge => {
+                return edge.node.name !== "404" && edge.node.name !== "index"
+              })
+              .map(edge => {
+                const link = `/${edge.node.name}`;
+                const name = edge.node.name;
+                return (
+                  <Link
+                    key={edge.node.id}
+                    to={link}
+                    className="navbar__item"
+                    activeClassName="active"
+                    partiallyActive={true}
+                  >
+                    {name}
+                  </Link>
+                )
+              })
+            }
+          </nav>
+        </header>
+      )}
+    />
+  )
+}
 
 export default Header
