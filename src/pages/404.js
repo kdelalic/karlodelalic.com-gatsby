6abcef4d7001a8c09da4c85f3f019351b7d3ece6
Karlo Delalic <karlo.delalic1@gmail.com>
2019-07-02 23:27:52 -0400

    Change 404 export default

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,11 +5,9 @@ import SEO from "../components/seo"
 
 import "./404.scss"
 
-const NotFoundPage = () => (
+export default () => (
   <Layout title="404" centered>
     <SEO title="404: Not found" />
     <h2 className="page-not-found">Page not found</h2>
   </Layout>
 )
-
-export default NotFoundPage
