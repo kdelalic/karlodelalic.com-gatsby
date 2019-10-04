073dc3e261cfdceed43141b5bf50d27865b1e3f8
Karlo Delalic <karlo.delalic@ibm.com>
2019-10-03 21:32:07 -0400

    More content

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ curl -s https://api.github.com/repos/kdelalic/auto-resume/releases/latest \
     | grep "browser_download_url" \
     | cut -d : -f 2,3 \
     | tr -d \" \
-    | wget -i - -P static
+    | wget -i - -P static --no-check-certificate
