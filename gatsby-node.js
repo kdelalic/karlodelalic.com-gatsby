b9a36e163b65ca5a212004415d6f0bb6b1935413
Karlo Delalic <karlo.delalic1@gmail.com>
2021-03-21 17:28:24 -0400

    Remove unused packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,5 @@
 const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
-const jsonld = require('jsonld')
-const { chromium } = require('playwright')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -52,43 +50,7 @@ exports.createPages = async ({ actions, graphql }) => {
             slug: node.fields.slug,
           },
         })
-      } else if (node.frontmatter.type === "recipe") {
-        // let recipe = await scrapeRecipe(node.frontmatter.source)
-        // console.log(recipe)
       }
     })
   })
-}
-
-const scrapeRecipe = async (url) => {
-  console.log('scraping url: ' + url)
-
-  const browser = await chromium.launch()
-  const context = await browser.newContext()
-  const page = await context.newPage()
-  await page.goto(url)
-
-  const handle = await page.$('script[type="application/ld+json"]')
-  if (handle === null) return
-  const text = await handle.innerText()
-
-  const schemaContext = {
-    recipe: 'http://schema.org/Recipe',
-    recipeIngredient: 'http://schema.org/recipeIngredient',
-  }
-
-  const compacted = await jsonld.compact(JSON.parse(text), schemaContext)
-
-  let ingredients = compacted.recipeIngredient
-
-  if (Array.isArray(compacted)) {
-    ingredients = compacted['@graph'].filter(
-      (item) => item['@type'] === 'recipe'
-    )[0].recipeIngredient
-  }
-
-  console.log(JSON.stringify(ingredients))
-
-  await browser.close()
-  return ingredients
 }
