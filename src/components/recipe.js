1ce807f17594375393ea4649288679f990669f3d
Karlo Delalic <karlo.delalic1@gmail.com>
2025-01-28 21:53:25 -0800

    Capitalize first letter

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -3,6 +3,12 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Chip from "./chip";
 import "./recipe.scss";
 
+const capitalizeTitle = (title = "") =>
+  title
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
 const Recipe = ({ image, source, tags, title, tagClick, tagFilters }) => {
   const tagFiltersSet = new Set(tagFilters);
 
@@ -12,7 +18,7 @@ const Recipe = ({ image, source, tags, title, tagClick, tagFilters }) => {
 
   return (
     <div className="recipe">
-      <h2 className="recipe__title">{title}</h2>
+      <h2 className="recipe__title">{capitalizeTitle(title)}</h2>
       <div className="recipe__body">
         {image && (
           <a
@@ -21,11 +27,15 @@ const Recipe = ({ image, source, tags, title, tagClick, tagFilters }) => {
             rel="nofollow noopener noreferrer"
             className="recipe__link"
           >
-            <GatsbyImage className="recipe__image" alt={title} image={getImage(image) ?? undefined} />
+            <GatsbyImage
+              className="recipe__image"
+              alt={title}
+              image={getImage(image) ?? undefined}
+            />
           </a>
         )}
         <div className="recipe__body__chips">
-          {tags.sort().map(tag => (
+          {tags.sort().map((tag) => (
             <Chip
               key={tag}
               label={tag}
